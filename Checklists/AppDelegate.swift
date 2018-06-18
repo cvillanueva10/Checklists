38c37300e098cdbb8fd09451cfb9c4119b6da6a8
Chris Villanueva <cvillanueva6@ucmerced.edu>
2018-06-18 10:17:58 -0700

    began local notifications

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +20,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (isGranted, error) in
+            if isGranted {
+                print("We got permission!")
+            } else {
+                print("No permission bruh")
+            }
+        }
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.tintColor = UIColor.init(red: 0, green: 107/255, blue: 180/255, alpha: 1)
